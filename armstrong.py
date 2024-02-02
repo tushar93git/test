dfeec5f2c53cc3222482416bb3841343c672c404
tushar93git <tushar.93d@gmail.com>
2024-02-02 11:26:24 +0530

    first commit

---
+++ armstrong.py
@@ -0,0 +1,16 @@
+def armstrong(num):
+    new = num
+    p = len(str(num))
+    result = 0
+    while num>0:
+        digit = num%10
+        result = result + digit**p
+        num = num//10
+    if result == new:
+        print("Armstrong")
+    else:
+        print("Not Armstrong")
+    
+    
+num = 153
+armstrong(num)
